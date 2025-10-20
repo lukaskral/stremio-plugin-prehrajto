3b390bcb671e0623c39826ade172b9451e70b24a
lukaskral <ahoj@lukaskral.eu>
2025-10-20 09:02:12 +0200

    fix

--- src/utils/getServerUrl.ts
+++ src/utils/getServerUrl.ts
@@ -1,4 +1,4 @@
 export function getServerUrl() {
-  const port = process.env.PORT ? Number(process.env.PORT) : 52932;
-  return `https://03df1f38e4c6-stremio-plugin-prehrajto.baby-beamup.club:${port}`;
+  // const port = process.env.PORT ? Number(process.env.PORT) : 52932;
+  return `https://03df1f38e4c6-stremio-plugin-prehrajto.baby-beamup.club`;
 }
